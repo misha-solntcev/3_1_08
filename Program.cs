a7e414c655ad618ba4d200b66242509ebd2d1ba2
misha-solntcev <misha.solntcev@gmail.com>
2022-11-27 09:05:05 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 8. Дано целое число N (> 0). Найти наибольшее целое число K, 
+    квадрат которого не превосходит N: K2 < N.
+    Функцию извлечения квадратного корня не использовать.*/
+
+namespace _3_1_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 102;
+            int K = 0;
+            while (K * K <= N)
+            {
+                Console.WriteLine(K);
+                K++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
